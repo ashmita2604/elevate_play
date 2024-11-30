402b8d53444ba9f834a3b16ed215aa869c687721
Ashmita Roy Karmakar <roykarmakar.ashmita@gmail.com>
2024-11-30 21:22:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Welcome message in console (optional)
+window.onload = function () {
+    console.log("Welcome to Elevate Play! The opening page is loaded.");
+  };
+  
+// Navigate to the main page
+function goToMainPage() {
+  // Redirect to main.html or dynamically load the main page content
+  window.location.href = "main.html";
+}
